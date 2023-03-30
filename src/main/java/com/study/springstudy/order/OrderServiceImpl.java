e357a31a8bffefd84e6ac15a2347901ef05cf098
JoshNoh <thnoh93@naver.com>
2023-03-30 14:29:29 +0900

    스프링 핵심 원리 이해2 - 변동 할인 정책 적용 및 문제점 발견(OCP,DIP 위반)

--- src/main/java/com/study/springstudy/order/OrderServiceImpl.java
+++ src/main/java/com/study/springstudy/order/OrderServiceImpl.java
@@ -2,14 +2,16 @@ package com.study.springstudy.order;
 
 import com.study.springstudy.discount.DiscountPolicy;
 import com.study.springstudy.discount.FixDiscountPolicy;
+import com.study.springstudy.discount.RateDiscountPolicy;
 import com.study.springstudy.member.Member;
 import com.study.springstudy.member.MemberRepository;
 import com.study.springstudy.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
 
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-
+    //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    //private DiscountPolicy discountPolicy;
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
     @Override
--- src/test/java/com/study/springstudy/order/OrderServiceTest.java
+++ src/test/java/com/study/springstudy/order/OrderServiceTest.java
@@ -14,11 +14,15 @@ public class OrderServiceTest {
 
     @Test
     void createOrder(){
+        // given
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(member);
 
+        // when
         Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        // then
         Assertions.assertEquals(order.getDiscountPrice(),1000);
 
 
