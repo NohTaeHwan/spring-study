db17870b0fbe97d03d0bbe3897c0817c8c76088e
JoshNoh <thnoh93@naver.com>
2023-04-09 22:44:51 +0900

    스프링 핵심 원리 이해2 - AppConfig 리팩터링

--- src/main/java/com/study/springstudy/AppConfig.java
+++ src/main/java/com/study/springstudy/AppConfig.java
@@ -1,6 +1,8 @@
 package com.study.springstudy;
 
+import com.study.springstudy.discount.DiscountPolicy;
 import com.study.springstudy.discount.FixDiscountPolicy;
+import com.study.springstudy.member.MemberRepository;
 import com.study.springstudy.member.MemberService;
 import com.study.springstudy.member.MemberServiceImpl;
 import com.study.springstudy.member.MemoryMemberRepository;
@@ -9,15 +11,23 @@ import com.study.springstudy.order.OrderServiceImpl;
 
 /**
  * AppConfig
- * 의존관계 주입
+ * 객체 생성 및 의존 관계 주입
  */
 public class AppConfig {
 
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService(){
-        return new OrderServiceImpl(new FixDiscountPolicy(), new MemoryMemberRepository());
+        return new OrderServiceImpl(discountPolicy(), memberRepository());
+    }
+
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
     }
 }
